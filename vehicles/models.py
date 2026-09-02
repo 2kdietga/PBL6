ef73beb4fc093632731c917cfd23d0aeb4b9f3e4
wind <wind@gmail.com>
2026-09-02 11:08:01 +0700

    thêm class VehicleTyoe

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class VehicleType(models.Model):
+    code = models.CharField(
+        max_length=20,
+        unique=True,
+    )
+
+    name = models.CharField(
+        max_length=100,
+    )
+
+    description = models.TextField(
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
