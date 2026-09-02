5c558886d1b8e8ab6c16448fde5676faeefd7530
wind <wind@gmail.com>
2026-09-02 10:51:07 +0700

    thêm class DriverProfile

--- accounts/models.py
+++ accounts/models.py
@@ -16,3 +16,31 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+class DriverProfile(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        APPROVED = "APPROVED", "Approved"
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="driver_profile",
+    )
+
+    full_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField()
+
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+
+    status = models.CharField(
+        max_length=10,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.full_name
